c34062a68d16cd9a2278e4600d0b013960cbf7f8
Elizabeth <tjeudy.e@gmail.com>
2023-07-13 14:52:21 -0400

    pushed emotionRadio function

--- index.js
+++ index.js
@@ -169,15 +169,28 @@ const catsData = [
 	},
 ];
 
+const emotionRadio = document.querySelector("#emotion-radios");
+
 // for of iterates over the value of property assigned to variable
 function getEmotionsArray(cats) {
-	const emotions = [];
+	const emotionsArr = [];
 	for (let cat of cats) {
 		// grabs emotions from array
 		for (let emotion of cat.emotionTags) {
-			emotions.push(emotion);
+			emotionsArr.push(emotion);
 		}
 	}
-	return emotions;
+	return emotionsArr;
 }
-getEmotionsArray(catsData);
+console.log(getEmotionsArray(catsData));
+
+// renders results of getEmotionsArray for radio input
+function renderEmotions(cats) {
+	let radioItems = ``;
+	const emotions = getEmotionsArray(cats);
+	for (let emotion of emotions) {
+		radioItems += `<p> ${emotion}</p>`;
+	}
+	emotionRadio.innerHTML = radioItems;
+}
+renderEmotions(catsData);

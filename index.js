55b225f6772de518d8d52b841c82d03793afccd6
Elizabeth <tjeudy.e@gmail.com>
2023-07-16 23:25:08 -0400

    highlight function styling js with classList / getElementByClassName

--- index.js
+++ index.js
@@ -1,14 +1,31 @@
-import { catsData } from "./data";
+import { catsData } from "./data.js";
 
 const emotionRadio = document.querySelector("#emotion-radios");
+
+// grabs the parent element of emotion and adds styling with classlist
+emotionRadio.addEventListener("change", highlightCheckedOption);
+function highlightCheckedOption(event) {
+	const radioArr = document.getElementsByClassName("radio");
+	for (let radio of radioArr) {
+		radio.classList.remove("highlight");
+	}
+
+	document
+		.getElementById(event.target.id)
+		.parentElement.classList.add("highlight");
+}
 
 // for of iterates over the value of property assigned to variable
 function getEmotionsArray(cats) {
 	const emotionsArr = [];
+
 	for (let cat of cats) {
 		// grabs emotions from array
 		for (let emotion of cat.emotionTags) {
-			emotionsArr.push(emotion);
+			// removed duplicates from cat data
+			if (!emotionsArr.includes(emotion)) {
+				emotionsArr.push(emotion);
+			}
 		}
 	}
 	return emotionsArr;
@@ -20,7 +37,20 @@ function renderEmotions(cats) {
 	let radioItems = ``;
 	const emotions = getEmotionsArray(cats);
 	for (let emotion of emotions) {
-		radioItems += `<p> ${emotion}</p>`;
+		radioItems += `
+		<div class="radios" id="radios">
+				<div class="radio">
+					<input 
+					type="radio"
+					id="${emotion}"
+					value="${emotion}"
+					name="emotion-radios"
+					>
+					<label for="${emotion}">${emotion}</label>
+				</div>
+			
+     </div>
+		`;
 	}
 	emotionRadio.innerHTML = radioItems;
 }

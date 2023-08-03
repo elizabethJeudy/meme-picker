b663dc12b06f06abd62e45586ec33ea14b045af8
Elizabeth <tjeudy.e@gmail.com>
2023-08-02 22:07:53 -0400

    checks animated gif is checked

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 import { catsData } from "./data.js";
 
-const emotionRadio = document.querySelector("#emotion-radios");
-const getImgBtn = document.querySelector("#get-image-btn");
+const emotionRadio = document.getElementById("emotion-radios");
+const getImgBtn = document.getElementById("get-image-btn");
+const animatedGifs = document.getElementById("gifs-only-option");
 
 emotionRadio.addEventListener("change", highlightCheckedOption);
-getImgBtn.addEventListener("clicked", getMatchingCatsArray);
+getImgBtn.addEventListener("click", getMatchingCatsArray);
 
 // grabs the parent element of emotion and adds styling with classlist
-
 function highlightCheckedOption(event) {
 	const radioArr = document.getElementsByClassName("radio");
 	for (let radio of radioArr) {
@@ -21,9 +21,14 @@ function highlightCheckedOption(event) {
 
 // checks if emotion has been clicked
 function getMatchingCatsArray() {
-	if (document.querySelector("input[type=radio]:checked")) {
+	const isGif = animatedGifs.checked;
+	if (isGif === true) {
+		console.log(animatedGifs);
+	}
+
+	if (document.querySelector('input[type="radio"]:checked')) {
 		const selectedEmotion = document.querySelector(
-			"input[type=radio]:checked"
+			'input[type="radio"]:checked'
 		).value;
 		console.log(selectedEmotion);
 	}
